bb6a264348d11501214bfb7df50562ec29109c9a
Jaem Dessources dit Dumesle <jaem.developer@gmail.com>
2026-09-16 23:46:44 -0400

    Updated malicious script

--- public/malicious-script.sh
+++ public/malicious-script.sh
@@ -1,1 +1,5 @@
 printf  "\e[31m┌────────────────────────────────────────────────────┐\n│ Alert: I just ran malicious script on your machine │\n└────────────────────────────────────────────────────┘\e[0m\n"
+
+touch malicious-file
+
+echo "I executed code to create a file on the remote computer" >> ./malicious-file
