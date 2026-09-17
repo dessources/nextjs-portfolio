8624aa57878a788fcb675e87663cfcbcd5c3881a
Jaem Dessources dit Dumesle <jaem.developer@gmail.com>
2026-09-16 23:55:05 -0400

    Updated malicious script to include name of attacker

--- public/malicious-script.sh
+++ public/malicious-script.sh
@@ -1,5 +1,5 @@
-printf  "\e[31m┌────────────────────────────────────────────────────┐\n│ Alert: I just ran malicious script on your machine │\n└────────────────────────────────────────────────────┘\e[0m\n"
+printf  "\e[31m┌────────────────────────────────────────────────────────────────────┐\n│ Alert: $1 just ran a malicious script on your machine             \n└────────────────────────────────────────────────────────────────────┘\e[0m\n"
 
 touch malicious-file
 
-echo "I executed code to create a file on the remote computer" >> ./malicious-file
+echo "$1 executed code to create a file on the remote computer" >> ./malicious-file
